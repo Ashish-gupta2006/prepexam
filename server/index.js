95714c09fdef314ea08c83b5a9fdda9130faf61b
Ashish-gupta2006 <ashishgupta16082@gmail.com>
2026-03-23 10:02:28 +0530

    update index.js file

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://prepexam.netlify.app/",
   }),
 );
 app.use(express.json());
